6d81e51ef21fc4ffed727b0db5136dddb410ac33
Tomasz Banas <tomasz.banas@sabre.com>
2016-12-01 08:59:33 +0100

    unused imports removed

--- src/ts/app.module.ts
+++ src/ts/app.module.ts
@@ -1,14 +1,12 @@
 import {AppComponent} from "./app.component";
 import "./rxjs-extensions";
 import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
 import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [AppComponent],
-    imports: [BrowserModule, FormsModule, HttpModule],
+    imports: [BrowserModule],
 })
 export class AppModule {
 }
