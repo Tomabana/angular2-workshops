6d1d6aead050f38371548e711fa70f939d5d56d7
Tomasz Banas <tomasz.banas@sabre.com>
2016-12-02 11:04:04 +0100

    lesson 7 - routing

--- src/ts/app.module.ts
+++ src/ts/app.module.ts
@@ -5,11 +5,29 @@ import {BrowserModule} from "@angular/platform-browser";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
+import {Routes, RouterModule} from "@angular/router";
+import {PostComponent} from "./post/post.component";
+
+const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "/dashboard",
+    },
+    {
+        component: DashboardComponent,
+        path: "dashboard",
+    },
+    {
+        component: PostComponent,
+        path: "post/:id",
+    },
+];
 
 @NgModule({
     bootstrap: [AppComponent],
-    declarations: [AppComponent, DashboardComponent],
-    imports: [BrowserModule, FormsModule, HttpModule],
+    declarations: [AppComponent, DashboardComponent, PostComponent],
+    imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(routes)],
 })
 export class AppModule {
 }
---
+++ src/ts/post/post.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "post",
+    templateUrl: "/src/html/post.html",
+})
+export class PostComponent {
+
+}
